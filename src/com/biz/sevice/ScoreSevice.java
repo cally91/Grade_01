c5f87eab6e788920e3ad3b9aa801c43acbafa11c
cally91 <cally91@naver.com>
2019-06-13 16:43:03 +0900

    f

--- src/com/biz/sevice/ScoreSevice.java
+++ src/com/biz/sevice/ScoreSevice.java
@@ -1,14 +1,12 @@
 package com.biz.sevice;
 
-public interface ScoreSevice{
+public interface ScoreSevice {
 
-public boolean inPutScore(int number) throws NumberFormatException ;
+	public boolean inPutScore(int number) throws NumberFormatException;
 
-	public void makeTotal() ;
-	
-	public void makeRank() ;
+	public void makeTotal();
 
-		
+	public void makeRank();
 
 	public void viewScore();
 }
